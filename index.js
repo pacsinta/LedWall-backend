6a09fc584d4fd7b4512d160ecb0b028446a7c333
Patrik Csikos <pacsinta@gmail.com>
2022-01-10 23:42:54 +0100

    https listenning added on port 4001

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const httpPORT = 4000;
+const httpsPORT = 4001;
 
 
 
 const express = require('express');
 const helmet = require('helmet');
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
 const routes = require('./routes');
 
 
@@ -19,7 +23,30 @@ app.use(express.json({ limit: "1kb" }));
 app.use(express.json());
 app.use(routes);
 
-app.listen(httpPORT, () => {
-    console.log(`LedWall-backend is running on http on worker ${process.pid}`);
-    console.log(`Port: ${httpPORT}`);
+
+//HTTP
+const httpServer = http.createServer(app);
+httpServer.listen(httpPORT, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+
+
+//HTTPS
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/cstcompany.ddns.net/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/cstcompany.ddns.net/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/cstcompany.ddns.net/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(httpsPORT, () => {
+	console.log(`LedWall-backend is running on http on worker ${process.pid}`);
+    console.log(`Port: ${httpsPORT}`);
 });
